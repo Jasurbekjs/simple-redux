60e2589ba5631a2bf81da04259f399f34aa9fa8f
Jasurbek <jasmirzaev@gmail.com>
2023-10-29 20:39:24 +0300

    createActions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
 
   const dispatch = useDispatch() // диспатсчер, функция изменяющая состояние
   const cash = useSelector(state=> state.cash.cash) // селектор для получения текущего состояния переменной
-  
+  const customers = useSelector(state=> state.customers.customers)
+
   const addCash = (cash) => {
     dispatch({type: "ADD_CASH", payload: cash})
   }
   const getCash = (cash) => {
     dispatch({type: "GET_CASH", payload: cash})
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+  const removeCustomer = (id) => {
+    dispatch(removeCustomerAction(id))
   }
 
   return (
@@ -23,6 +36,23 @@ function App() {
           <button onClick={()=>addCash( Number(prompt()) )}>Пополнить счет</button>
           <button onClick={()=>getCash( Number(prompt()) )}>Снять со счета</button>
         </div>
+        <div>
+          <button onClick={()=>addCustomer( prompt() )}>Добавить клиента</button>
+        </div>
+        {customers.length > 0 ? 
+          <div>
+            {customers.map(customer => 
+                <div key={customer.id}>
+                  <span> {customer.name} </span>
+                  <button onClick={()=>removeCustomer( customer.id )}>Удалить клиента</button> 
+                </div>
+            )}
+          </div>
+          : 
+          <div style={{fontSize: '2rem', marginTop: 20}}>
+            Клиенты отсутствуют!
+          </div>
+        }
       </div>
     </>
   );
